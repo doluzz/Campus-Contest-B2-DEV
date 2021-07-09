fa515191d50519629793e51eec84214e78de0f86
Dorian Auneau <dorianauneau@gmail.com>
2021-07-09 15:48:54 +0200

    _

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Campus-Contest-B2-DEV
+Il n'y a rien de concret mais pour tester il faut appeller chacune des fonctions.
---
+++ classes/Block.py
@@ -0,0 +1,24 @@
+class Block:
+    base_hash = ""
+    hash = ""
+    parent_hash = ""
+    transactions = ""
+
+def check_hash():
+    pass
+
+def add_transaction():
+    pass
+
+def get_transaction():
+    pass
+
+def get_weight():
+    pass
+
+def save():
+    pass
+
+def load():
+    pass
+
